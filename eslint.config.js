fbedab1ddac3c1dbaa7459e1e7dc9b49e2580312
Alex <lanchado10@gmail.com>
2025-08-07 11:51:38 -0500

    Proyecto base con CI

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+
+        },
+        rules: {
+            // uso de punto y coma
+            'semi': ['error', 'always'],
+            // uso de comillas simples
+            'quotes': ['error', 'single'],
+        }
+    }
+]
---
+++ index.js
@@ -0,0 +1,18 @@
+//servidor
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT;
+
+//endpoint que responde un mensaje
+
+app.get('/', (_req,res) => {
+    res.send('Integracion continua funcionando');       
+});
+
+app.listen (PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+}   );
+
+
+
